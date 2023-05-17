9b00db31202abfbc6d6a979f5e737624263564be
frost-secu <shw9810@icloud.com>
2023-05-17 23:27:00 +0900

    추가 : 프로젝트화 & Tomcat 모듈 import 수정 : 일부 경로 및 import 모듈 구문 Blind 처리 : context-param URL & Pwd

--- Back_end/JSPLine/src/setAccount/accountDAO.java
+++ Back_end/JSPLine/src/setAccount/accountDAO.java
@@ -1,4 +1,6 @@
+package setAccount;
 
+import common.JDBConnect;
 
 public class accountDAO extends JDBConnect {
 	public accountDAO(String drv, String url, String id, String pwd) {
@@ -7,7 +9,7 @@ public class accountDAO extends JDBConnect {
 
 	public accountDTO getAccountDTO(String uid, String upwd) {
 		accountDTO dto = new accountDTO();
-		String query = "select * from acct where ID=? and PW=?";
+		String query = "select * from ACCNT where ID=? and PW=?";
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, uid);
@@ -15,11 +17,11 @@ public class accountDAO extends JDBConnect {
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				dto.setId(rs.getString("ID"));
-				dto.setPw(rs.getString("PW"));
+				dto.setPwd(rs.getString("PW"));
 				dto.setName(rs.getString("NAME"));
-				dto.setAccpw(res.getString("ACCPW"));
-				dto.setAccount(res.getString("Account"));
-				dto.setMoney(res.getString("MONEY"));
+				dto.setAccPwd(rs.getString("ACCPW"));
+				dto.setAccount(rs.getString("Account"));
+				dto.setMoney(rs.getString("MONEY"));
 			}
 		} catch (Exception e) {
 			System.out.println("Exception [Get Account DTO] : " + e.getMessage());
---
+++ Back_end/JSPLine/src/setAccount/accountDTO.java
@@ -0,0 +1,59 @@
+package setAccount;
+
+public class accountDTO {
+	private String name;
+	private String id;
+	private String pwd;
+	private String accPwd;
+	private String account;
+	private String money;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getAccPwd() {
+		return accPwd;
+	}
+
+	public void setAccPwd(String accPwd) {
+		this.accPwd = accPwd;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+
+	public void setMoney(String money) {
+		this.money = money;
+	}
+
+}
